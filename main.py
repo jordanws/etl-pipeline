1b26629cc6bc7edc1b1c4398ea2a2d3b061b5a4b
Jowi <93743976+jordanws@users.noreply.github.com>
2026-07-16 19:55:10 +0000

    add clients, products and values

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ cursor.execute("""
     );
 """)
 
+cursor.execute("DELETE FROM pedidos")
+cursor.execute("DELETE FROM clientes")
+
+cursor.execute("INSERT INTO clientes (nome) VALUES (?)", ('João Silva',))
+cursor.execute("INSERT INTO clientes (nome) VALUES (?)", ('Maria Santos',))
+
+cursor.execute("INSERT INTO pedidos (id_cliente, produto, preco) VALUES (?, ?, ?)", (1, 'Notebook', 4000.00))
+cursor.execute("INSERT INTO pedidos (id_cliente, produto, preco) VALUES (?, ?, ?)", (2, 'Mouse sem fio', 120.50))
+cursor.execute("INSERT INTO pedidos (id_cliente, produto, preco) VALUES (?, ?, ?)", (1, 'Computador(Desktop)', 5600.00))
+
+
 conexaoBd.commit()
 conexaoBd.close()
 
